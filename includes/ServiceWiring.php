6d14104ac34c0bc02c19f5cab85b7db305f0aacd
Dreamy Jazz <dreamyjazzwikipedia@gmail.com>
2022-12-23 23:40:53 +0000

    Test ServiceWiring.php
    Test ServiceWiring.php using tests copied from CentralAuth. Because
    phpunit does not support marking a file as covered, the ServiceWiring
    file is ignored for code coverage as the tests fully cover the file.
    
    Add TestAutoloadNamespaces to extension.json which will autoload
    phpunit test files for this extension.
    
    Change-Id: I03de2a0d15fffb4a0b16f1efa42602aa6006d7fb

--- includes/ServiceWiring.php
+++ includes/ServiceWiring.php
@@ -2,8 +2,15 @@
 
 use MediaWiki\MediaWikiServices;
 
+// PHP unit does not understand code coverage for this file
+// as the @covers annotation cannot cover a specific file
+// This is fully tested in ServiceWiringTest.php
+// @codeCoverageIgnoreStart
+
 return [
 	'BSSocialAutoWatcherFactory' => static function ( MediaWikiServices $services ) {
 		return new \BlueSpice\Social\Watch\AutoWatcherFactory();
 	}
 ];
+
+// @codeCoverageIgnoreEnd
